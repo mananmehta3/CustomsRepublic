dbc6673a95fb2cc0aa1546f9cad6490628c95f2e
Manan Mehta <Manan.Mehta1@ibm.com>
2023-02-25 18:59:21 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Customs Republic</a>
+      </header>
+      <main>Products</main>
+    </div>
+  );
+}
+
+export default App;
